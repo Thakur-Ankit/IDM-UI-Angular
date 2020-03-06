f41df1b415c5352f5233e5a898aeb30b5daf607c
Ankit Thakur <tankit541@gmail.com>
2020-03-06 11:57:03 +0530

    Complete Users Component dashboard.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {UserService} from '../services/user.service';
+
+export interface UserData {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  status: boolean;
+}
 
 @Component({
   selector: 'app-users',
@@ -8,10 +18,44 @@ import {Router} from '@angular/router';
 })
 export class UsersComponent implements OnInit {
 
-  constructor(private router: Router) {
+  @ViewChild(MatSort, null) sort: MatSort;
+  @ViewChild(MatPaginator, null) paginator: MatPaginator;
+
+  displayedColumns: string[] = ['id', 'name', 'email', 'role', 'status', 'edit'];
+  customDataSource;
+  userData: UserData[] = [];
+
+  constructor(private router: Router, private userService: UserService) {
   }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getUsers()
+      .subscribe(res => {
+        console.log(res);
+        res.forEach(user => {
+          this.userData.push(this.convertToUserData(user));
+        });
+        this.customDataSource = new MatTableDataSource(this.userData);
+        this.customDataSource.sort = this.sort;
+        this.customDataSource.paginator = this.paginator;
+      }, err => {
+        console.log(err);
+        throw err;
+      });
+  }
+
+  convertToUserData(user): UserData {
+    return {
+      id: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.email,
+      role: user.userType,
+      status: user.status
+    };
   }
 
   navigate(location: string) {
